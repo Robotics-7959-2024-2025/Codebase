828e267a6ae82b2e822dffb1c46f96d13e23e642
kesoc <keirsampson@gmail.com>
2024-10-06 17:46:57 -0400

    test commit

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -7,6 +7,8 @@ import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
 import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import java.util.Vector;
+
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(500);
@@ -15,16 +17,8 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVez`l, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(60, -12, (3.14159)))
-                        .forward(25)
-                        .strafeLeft(7.5)
-                        .turn(Math.toRadians(80))
-                        .splineTo(new Vector2d(-35,-34), Math.toRadians(180))
-                        .strafeLeft(15)
-                        .forward(5)
-                        .back(10)
-                        .turn(Math.toRadians(135))
-                        .strafeLeft(5)
-                        .forward(20)
+                        .splineTo(new Vector2d(-58, -58), Math.toRadians(225))
+                        .waitSeconds(10)
                         .build());
 
 
