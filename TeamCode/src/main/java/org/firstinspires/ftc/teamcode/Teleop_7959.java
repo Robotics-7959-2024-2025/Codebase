87adf416e92a91be88f107e171f25463476e8f11
kesoc <keirsampson@gmail.com>
2024-10-24 16:34:33 -0400

    put the up mechanism but commented it out

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_7959.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_7959.java
@@ -7,14 +7,13 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name="Teleop 7959")
+@TeleOp(name="Teleop 7959 (with outtake)")
 public class Teleop_7959 extends LinearOpMode {
 
     private DcMotor leftFront;
     private DcMotor rightFront;
     private DcMotor leftBack;
     private DcMotor rightBack;
-    private DcMotorEx liftR;
     private DcMotorEx liftL;
 
     //32 bit decimals are floats, while 64 bit are doubles, thus, doubles are better.
@@ -32,11 +31,9 @@ public class Teleop_7959 extends LinearOpMode {
         rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
         rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
         liftL = hardwareMap.get(DcMotorEx.class, "liftL");
-        liftR = hardwareMap.get(DcMotorEx.class, "liftR");
 
-        //as long as lifts are the different direction the individual direction doesnt matter
         liftL.setDirection(DcMotor.Direction.FORWARD);
-        liftR.setDirection(DcMotor.Direction.REVERSE);
+
         leftFront.setDirection(DcMotor.Direction.FORWARD);
         rightFront.setDirection(DcMotor.Direction.REVERSE);
         leftBack.setDirection(DcMotor.Direction.FORWARD);
@@ -55,20 +52,14 @@ public class Teleop_7959 extends LinearOpMode {
             rightBack.setPower(gamepad1.right_stick_x);
             leftBack.setPower(-gamepad1.right_stick_x);
 
-            /*
-            if(gamepad1.left_trigger > 0 && !liftL.isBusy() && !liftR.isBusy()) {
+
+            if(gamepad1.left_trigger > 0 && !liftL.isBusy()) {
                 //moves the arm up
                 telemetry.addData("ARM UP", "true");
                 telemetry.update();
-                liftR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                liftR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                 //tickcount is 537
                 //1460 is about 2.5 revolutions or whatever 1460/537 is
                 //537 encoder ticks = 360 degrees on a motor
-                liftR.setTargetPosition(400);
-                liftR.setDirection(DcMotor.Direction.REVERSE);
-                liftR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                liftR.setPower(1);
 
                 liftL.setTargetPosition(400);
                 liftL.setDirection(DcMotor.Direction.FORWARD);
@@ -76,23 +67,19 @@ public class Teleop_7959 extends LinearOpMode {
                 liftL.setPower(1);
             }
 
-            if(gamepad1.right_trigger > 0 && !liftL.isBusy() && !liftR.isBusy()){
+            if(gamepad1.right_trigger > 0 && !liftL.isBusy()){
                 //moves the arm down
-                telemetry.addData("rt working", true);
+                telemetry.addData("ARM DOWN", "true");
                 telemetry.update();
+
 
                 liftL.setTargetPosition(0);
                 liftL.setDirection(DcMotor.Direction.REVERSE);
                 liftL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 liftL.setPower(-0.5);
-
-                liftR.setTargetPosition(0);
-                liftR.setDirection(DcMotor.Direction.FORWARD);
-                liftR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                liftR.setPower(-0.5);
             }
 
-            */
+
 
 
 
